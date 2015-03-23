1caa9041ce15443f106a7290947a2d00464ea43b
luis perera <luanup.pro@gmail.com>
2015-03-23 16:00:18 -0600

    carpeta completa

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+$config_file = 'config.ini';
+
+$options = parse_ini_file($config_file, True);
+foreach($options as $section=>$config) {
+    if($section != 'PLUGINS') {
+        foreach($config as $constant=>$value) {
+            define($constant, $value);
+            if($section == 'TEMPLATE') $GLOBALS['DICT'][$constant] = $value;
+        }
+    }
+}
+
+if(!PRODUCTION) {
+    ini_set('error_reporting', E_ALL | E_NOTICE | E_STRICT);
+    ini_set('display_errors', '1');
+    ini_set('track_errors', 'On');
+} else {
+    ini_set('display_errors', '0');
+}
+
+function import($str='', $exit=True) {
+    $file = str_replace('.', '/', $str);
+    if(file_exists(APP_DIR . "$file.php")) {
+        require_once "$file.php";
+    } else {
+        if($exit) exit("FATAL ERROR: no se pudo importar '$str'");
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-require_once 'settings.php';
+require_once 'config.php';
+
+import('core.Controller');
+import('core.Model');
+import('core.View');
+import('core.Request');
+import('core.Database');
+
+#$con = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset='.DB_CHARSET, DB_USER, DB_PASS);
+
 ?>
