106bde1898fb1814d671e6a4075032821d4e07a7
luis perera <luanup.pro@gmail.com>
2015-03-25 16:02:09 -0600

    vista modificada para añadir los campos necesarios

--- models/registroModel.php
+++ models/registroModel.php
@@ -3,6 +3,32 @@ class registroModel extends Model{
     public function __construct() {
         parent::__construct();
     }
+
+    public function getTipos(){
+        $data = array('TABLE'=>'tipos','COLUMNS'=>'*');
+    	$select = $this->_dbf->get_select_query($data);
+        $q = $this->_db->query($select);
+        $string = "<option value='0'>Elige...</option>";
+        
+        if(count($q->fetch(PDO::FETCH_ASSOC)) > 0){
+            foreach ($q as $row){
+                $string .= "<option value='".$row['id']."'>".$row['nombre']."</option>";
+            }
+        }
+        
+        return $string;
+    }
     
+    function setusuario($arr,$values){
+        $insert = $this->_dbf->get_insert_query($arr);
+        $q = $this->_db->prepare($insert);
+        $q->execute($values);
+        
+        return $q->lastInsertId();
+    }
+    
+    function getUsuario(){
+        
+    }
 }
 ?>
