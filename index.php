923eb0b3a8d0b4229d36cecd8b2691a75e19a976
luis perera <luanup.pro@gmail.com>
2015-03-24 21:49:04 -0600

    archivo index con try y catch

--- index.php
+++ index.php
@@ -9,17 +9,13 @@ require_once 'config.php';
     import('core.Session');
     import('core.Database');
     
-
+try{
         Session::init();
         Autoload::run(new Request);
-<<<<<<< HEAD
-    
-=======
         
     } catch (Exception $exc) {
         echo $exc->getMessage();
         echo "Hey";
     }
->>>>>>> 2f480d80a04c7399f830041da0fa711f1f9a4091
 
 ?>
