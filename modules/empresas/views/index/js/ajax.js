0b257e5d7b17294057d31f8afec6432d3483bb3f
Sistemas Ariuss <sistemas@ariuss.net>
2015-04-06 18:09:17 -0500

    ajax de emprsas

--- modules/empresas/views/index/js/ajax.js
+++ modules/empresas/views/index/js/ajax.js
@@ -1,23 +1,3 @@
-function modificarEmpresas(id,tipo,form){
-	if(form != false)
-		var form = $('#'+form).serialize();
-	$.ajax({
-		url: 'empresas/'+tipo,
-		type: 'POST',
-		data: {id: id,form:form},
-	})
-	.done(function() {
-		console.log("success");
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
-	});
-	
-}
-
 $(document).ready(function() {
 	$('.editar').click(function(event) {
 		var id = $(this).attr('valor');
@@ -35,5 +15,9 @@ $(document).ready(function() {
 		var id = $(this).attr('valor');
 		$('#idEmpresa').val(id);
 		$('#desactivar').modal('show');
+	});
+
+	$('#btnDesactivar').click(function(e) {
+		
 	});
 });
